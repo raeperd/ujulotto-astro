187911158b241b064f9160f3b4165d5db9ea3846
raeperd <raeperd117@gmail.com>
2024-02-25 13:30:07 +0900

    chore: refactor config with import order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import auth from 'auth-astro';
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import auth from 'auth-astro'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), auth()],
-  adapter: vercel()
-});
+  integrations: [tailwind({ applyBaseStyles: false }), react(), auth()],
+  adapter: vercel(),
+})
