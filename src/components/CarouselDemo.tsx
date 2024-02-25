e47fa866b4333e05bf72ce006dab2fbfe3c8cafa
raeperd <raeperd117@gmail.com>
2024-02-25 12:20:39 +0900

    feat: Add WeeklyWinnerCarousel

--- src/components/CarouselDemo.tsx
+++ src/components/CarouselDemo.tsx
@@ -4,6 +4,7 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel'
 import Autoplay from 'embla-carousel-autoplay'
+import NumberBall from './NumberBall'
 
 export function CarouselDemo() {
   return (
@@ -28,7 +29,7 @@ export function CarouselDemo() {
 function LocationCarouselItem() {
   return (
     <CarouselItem>
-      <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl">
+      <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl h-[90px]">
         <h2 className="text-lg font-semibold">복권 판매점 쉽게 찾기</h2>
         <p className="text-xs mt-1">전국 1등 판매점</p>
       </div>
@@ -39,9 +40,27 @@ function LocationCarouselItem() {
 function WeeklyWinnerCarousel() {
   return (
     <CarouselItem>
-      <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl">
-        <h2 className="text-lg font-semibold">복권 판매점 쉽게 찾기</h2>
-        <p className="text-xs mt-1">전국 1등 판매점</p>
+      <div className="bg-[#e2e3ff] h-[90px] rounded-[14px] py-3 px-5 text-black">
+        <h2 className="font-semibold  text-base">
+          <p className="inline mr-1 bg-point text-white rounded-[14px] font-bold text-sm py-1 px-3">
+            1089회
+          </p>
+          이번주 추첨 번호
+        </h2>
+        <div className="flex items-center gap-1.5 mt-2.5">
+          {[1, 11, 22, 33, 44, 33].map((num, index) => (
+            <NumberBall
+              key={index}
+              number={num}
+              width={24}
+            />
+          ))}
+          +
+          <NumberBall
+            number={44}
+            width={24}
+          ></NumberBall>
+        </div>
       </div>
     </CarouselItem>
   )
---
+++ src/components/NumberBall.tsx
@@ -0,0 +1,41 @@
+import { type ComponentPropsWithoutRef } from 'react'
+
+export default function NumberBall(props: NumberBallProps) {
+  const { number, width, background, color, className, ...rest } = props
+  const colorFromNumber = (num: number) => {
+    if (num < 11) {
+      return 'linear-gradient(143deg, #FFCD12 16.55%, #FFF2AB 71.22%)'
+    }
+    if (num < 21) {
+      return 'linear-gradient(143deg, #5F88F1 16.55%, #9BB2FB 71.22%)'
+    }
+    if (num < 31) {
+      return 'linear-gradient(143deg, #FF6969 16.55%, #FEB1B1 71.22%)'
+    }
+    if (num < 41) {
+      return 'linear-gradient(143deg, #636262 16.55%, #949494 71.22%)'
+    }
+    return 'linear-gradient(143deg, #3CE038 16.55%, #9BFA98 70.85%)'
+  }
+  return (
+    <div
+      className={`${className} aspect-square  rounded-full text-center leading-loose`}
+      style={{
+        width: width,
+        fontSize: width / 2,
+        background: background ?? colorFromNumber(number),
+        color: color ?? 'black',
+      }}
+      {...rest}
+    >
+      {number}
+    </div>
+  )
+}
+
+interface NumberBallProps extends ComponentPropsWithoutRef<'div'> {
+  number: number
+  width: number
+  background?: string
+  color?: string
+}
