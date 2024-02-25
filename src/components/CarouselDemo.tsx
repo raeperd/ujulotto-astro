d44e21d0ba1599cde41c314ee6f6bf4d7b1cd799
raeperd <raeperd117@gmail.com>
2024-02-25 11:18:30 +0900

    refactor: CarouselDemo

--- src/components/CarouselDemo.tsx
+++ src/components/CarouselDemo.tsx
@@ -8,7 +8,7 @@ import Autoplay from 'embla-carousel-autoplay'
 export function CarouselDemo() {
   return (
     <Carousel
-      className="w-full max-w-xs"
+      className="w-full max-w-xs mt-5"
       opts={{ loop: true }}
       plugins={[
         Autoplay({
@@ -17,15 +17,32 @@ export function CarouselDemo() {
       ]}
     >
       <CarouselContent>
-        {Array.from({ length: 2 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl">
-              <h2 className="text-lg font-semibold">복권 판매점 쉽게 찾기</h2>
-              <p className="text-xs mt-1">전국 1등 판매점</p>
-            </div>
-          </CarouselItem>
+        {[LocationCarouselItem, WeeklyWinnerCarousel].map((Item, index) => (
+          <Item key={index} />
         ))}
       </CarouselContent>
     </Carousel>
+  )
+}
+
+function LocationCarouselItem() {
+  return (
+    <CarouselItem>
+      <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl">
+        <h2 className="text-lg font-semibold">복권 판매점 쉽게 찾기</h2>
+        <p className="text-xs mt-1">전국 1등 판매점</p>
+      </div>
+    </CarouselItem>
+  )
+}
+
+function WeeklyWinnerCarousel() {
+  return (
+    <CarouselItem>
+      <div className="bg-[url(/carousel-location.png)] bg-cover py-4 px-5 rounded-xl">
+        <h2 className="text-lg font-semibold">복권 판매점 쉽게 찾기</h2>
+        <p className="text-xs mt-1">전국 1등 판매점</p>
+      </div>
+    </CarouselItem>
   )
 }
