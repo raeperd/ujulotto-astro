3e35d839d9ecd564ab1da74f1db7227daaa7250b
raeperd <raeperd117@gmail.com>
2024-02-29 21:55:38 +0900

    refactor: Add textFromMode type

--- src/components/NumberPickCarousel.tsx
+++ src/components/NumberPickCarousel.tsx
@@ -1,6 +1,7 @@
 import { type CarouselApi } from '@/components/ui/carousel'
 import { useEffect, useState, type ComponentPropsWithoutRef } from 'react'
 
+import { textFromMode, type GenerationMode } from '../lib/type'
 import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
 
 export default function NumberPickCarousel(
@@ -31,7 +32,7 @@ export default function NumberPickCarousel(
             onClick={() => api?.scrollTo(i)}
             key={i}
           >
-            {v.name}
+            {textFromMode(v.mode)}
           </button>
         ))}
       </nav>
@@ -44,16 +45,16 @@ export default function NumberPickCarousel(
           {modes.map((v, i) => (
             <CarouselItem key={i}>
               <article className="bg-black px-5 py-[30px] rounded-[14px]">
-                <h2 className="font-bold text-xl">{v.name}</h2>
+                <h2 className="font-bold text-xl">{textFromMode(v.mode)}</h2>
                 <p className="mt-1.5 h-[2em]">{v.description}</p>
                 <img
                   className="w-[calc(100%-20px)] mx-auto mt-9 mb-4"
                   src={v.cover}
-                  alt={`cover ${v.name}`}
+                  alt={`cover ${v.mode}`}
                 />
-                <a href={`/numbers/${v.name}`}>
+                <a href={`/numbers/${v.mode}`}>
                   <div className="text-black bg-white text-center w-full font-bold text-base py-[14px] rounded-[4px]">
-                    {v.name == '직접조합' ? '직접 뽑기' : '번호 뽑기'}
+                    {v.mode == 'self' ? '직접 뽑기' : '번호 뽑기'}
                   </div>
                 </a>
               </article>
@@ -65,34 +66,34 @@ export default function NumberPickCarousel(
   )
 }
 
-const modes = [
+const modes: { mode: GenerationMode; description: string; cover: string }[] = [
   {
-    name: '직접조합',
+    mode: 'self',
     description: '6개의 모든 번호를 직접 뽑아 조합하는 방식이에요.',
     cover: '/cover-custom-pick.png',
   },
   {
-    name: '랜덤뽑기',
+    mode: 'random',
     description: '최대 6개 번호를 무작위로 랜덤 추첨합니다.',
     cover: '/cover-random-pick.png',
   },
   {
-    name: '우주추천',
+    mode: 'universe',
     description: '당첨번호와 선호 번호 생성 데이터에 기반한 추천 방식이에요.',
     cover: '/cover-universe-pick.png',
   },
   {
-    name: '미출현 번호',
+    mode: 'missing',
     description: '한달 간 추첨되지 않은 번호 조합이에요.',
     cover: '/cover-missing-pick.png',
   },
   {
-    name: '짝홀조합',
+    mode: 'odd-even',
     description: '짝수, 홀수 조합으로 추첨해드려요.',
     cover: '/cover-odd-even-pick.png',
   },
   {
-    name: '1등 번호기반',
+    mode: 'winner-based',
     description: '1등 번호를 기반으로 추첨해드려요.',
     cover: '/cover-winner-pick.png',
   },
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,12 +1,29 @@
 import { z } from 'astro/zod'
 
 export const generationModeSchema = z.enum([
-  '직접조합',
-  '랜덤뽑기',
-  '짝홀조합',
-  '우주추천',
-  '미출현 번호',
-  '1등 번호기반',
+  'self',
+  'random',
+  'odd-even',
+  'universe',
+  'missing',
+  'winner-based',
 ])
+
+export const textFromMode = (mode: GenerationMode) => {
+  switch (mode) {
+    case 'self':
+      return '직접조합'
+    case 'random':
+      return '랜덤뽑기'
+    case 'odd-even':
+      return '짝홀조합'
+    case 'universe':
+      return '우주추천'
+    case 'missing':
+      return '미출현 번호'
+    case 'winner-based':
+      return '1등 번호기반'
+  }
+}
 
 export type GenerationMode = z.infer<typeof generationModeSchema>
