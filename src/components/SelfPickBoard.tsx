c07b646bcdfe2141cc365021fac56e210afefd90
raeperd <raeperd117@gmail.com>
2024-03-01 01:55:42 +0900

    feat: Add navigation between rows in SelfPickBoard

--- src/components/SelfPickBoard.tsx
+++ src/components/SelfPickBoard.tsx
@@ -60,34 +60,72 @@ export default function SelfPickBoard() {
           </div>
         ))}
       </div>
-      <div className="bg-[#242429] grid grid-cols-7 mt-2.5 px-5 pt-5 pb-[34px] rounded-[14px] gap-2">
-        {Array.from({ length: 45 }).map((_, i) => (
+      <div className="mt-2.5 relative bg-[#242429] px-5 pt-5 pb-[34px] rounded-[14px]">
+        <div className=" grid grid-cols-7 gap-2">
+          {Array.from({ length: 45 }).map((_, i) => (
+            <button
+              key={i}
+              onClick={() => {
+                const included = numbers[index].includes(i + 1)
+                setNumbers((prev) => {
+                  const next = [...prev]
+                  next[index] = included
+                    ? prev[index].filter((number) => number !== i + 1)
+                    : [...prev[index], i + 1]
+                  return next
+                })
+              }}
+              disabled={
+                numbers[index].length == 12 &&
+                numbers[index].includes(i + 1) == false
+              }
+            >
+              <NumberBall
+                className="font-bold"
+                width={34}
+                number={i + 1}
+                background={
+                  numbers[index].includes(i + 1) ? 'white' : '#1b1c20'
+                }
+                color={numbers[index].includes(i + 1) ? '#1b1c20' : 'white'}
+              ></NumberBall>
+            </button>
+          ))}
+        </div>
+        <div className="absolute flex gap-2.5 bottom-5 right-5">
           <button
-            key={i}
+            className="bg-[#474747] rounded-[4px] py-1.5 px-4"
             onClick={() => {
-              const included = numbers[index].includes(i + 1)
               setNumbers((prev) => {
                 const next = [...prev]
-                next[index] = included
-                  ? prev[index].filter((number) => number !== i + 1)
-                  : [...prev[index], i + 1]
+                if (next[index].length == 12) {
+                  next[index] = Array.from({ length: 6 }, () => 46)
+                }
+                next[index] = [
+                  ...next[index],
+                  ...Array.from(
+                    { length: 12 - next[index].length },
+                    () => Math.floor(Math.random() * 45) + 1,
+                  ),
+                ]
                 return next
               })
+              setIndex((prev) => (prev + 1) % 5)
             }}
-            disabled={
-              numbers[index].length == 12 &&
-              numbers[index].includes(i + 1) == false
-            }
           >
-            <NumberBall
-              className="font-bold"
-              width={34}
-              number={i + 1}
-              background={numbers[index].includes(i + 1) ? 'white' : '#1b1c20'}
-              color={numbers[index].includes(i + 1) ? '#1b1c20' : 'white'}
-            ></NumberBall>
+            자동
           </button>
-        ))}
+          <button
+            className={`flex items-center gap-3 bg-[#474747] rounded-[4px] py-1.5 px-4 ${
+              numbers[index].length == 12 ? '' : 'opacity-40'
+            }`}
+            onClick={() => setIndex((prev) => (prev + 1) % rowLabels.length)}
+            disabled={numbers[index].length != 12}
+          >
+            다음
+            <IconNext></IconNext>
+          </button>
+        </div>
       </div>
     </article>
   )
@@ -121,6 +159,23 @@ const IconRefresh = (props: SVGProps<SVGSVGElement>) => (
     <path
       fillRule="evenodd"
       d="M7.97 0C12.405 0 16 3.582 16 8s-3.595 8-8.03 8a8.03 8.03 0 0 1-7.532-5.22.94.94 0 0 1 .558-1.21.946.946 0 0 1 1.214.555 6.142 6.142 0 0 0 5.76 3.993c3.391 0 6.14-2.74 6.14-6.118 0-3.379-2.749-6.118-6.14-6.118A6.136 6.136 0 0 0 3.12 4.25h2.077l.073.003c.488.037.871.443.871.938a.943.943 0 0 1-.877.94l-.067.001H2.662l-.11-.002a2.657 2.657 0 0 1-2.55-2.553L0 3.479V.955L.003.882A.943.943 0 0 1 .945.014c.499 0 .907.386.942.874l.002.067v1.82A8.027 8.027 0 0 1 7.97 0Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+const IconNext = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={8}
+    height={14}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M.243 1.899c-.353-.322-.317-.816.08-1.102.38-.274.952-.26 1.31.025l.047.04L7.7 6.36c.4.364.4.914 0 1.278l-6.02 5.499c-.353.322-.96.351-1.357.065-.38-.274-.43-.737-.124-1.06l.044-.042 5.352-4.888a.278.278 0 0 0 0-.426L.243 1.899Z"
       clipRule="evenodd"
     />
   </svg>
