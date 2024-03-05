42ef03499d2be4f9422864592fc3093ebdc96ef0
raeperd <raeperd117@gmail.com>
2024-03-05 22:56:42 +0900

    feat: Add pwa config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import AstroPWA from '@vite-pwa/astro'
 import { defineConfig } from 'astro/config'
 import auth from 'auth-astro'
 
@@ -8,6 +9,45 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind({ applyBaseStyles: false }), react(), auth()],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    auth(),
+    AstroPWA({
+      manifest: {
+        name: '우주로또',
+        short_name: '우주로또',
+        start_url: '/',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   adapter: vercel(),
 })
